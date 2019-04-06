90d4ce652348a32723335d107416e52510e31cb1
pedrobarata <pedrofelipebarata@gmail.com>
2019-04-06 17:32:13 -0300

    feat: refatoração e comando digdon

--- index.js
+++ index.js
@@ -11,15 +11,21 @@ bot.on("message", async message => {
     const prefix = botconfig.prefix;
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
-  if (cmd === `${prefix}vemtranquilo`) {
-    message.channel.send("( •_•) \n ( ง )ง \n /︶\\ vem tranquilo, vem tranquilo...");
-    message.channel.send('( •_•) \n ( ง )ง \n /︶\\ vem afobado não... vem tranquilo');
-    message.channel.send('( •_•) \n ( ง )ง \n /︶\\ eu e tu...eu e tu ');
-    return;
-  }
-  if(cmd === `${prefix}vemprofut`) {
-      message.channel.send(`Vem pro fut vem⚽Vem pro fut ⚽#VemProFut ⚽😎O futebol óh⚽tá estralando⚽😎tá pegado ò🤙✌️só tapa👋🤞só tapa de qualidade monstra🦕🐉aí o desafio Thiago Nere🤙🤙⚽é nós👉tamo junto⚽😎sempre🤙✌️lado a lado⚽aí eu tô de seleção⚽a satisfação aqui é total 😎🤙tamo junto⚽Vem`)
-  }
-})
+  switch (cmd) {
+    case `${prefix}vemtranquilo`: 
+        message.channel.send("( •_•) \n ( ง )ง \n /︶\\ vem tranquilo, vem tranquilo...");
+        message.channel.send('( •_•) \n ( ง )ง \n /︶\\ vem afobado não... vem tranquilo');
+        message.channel.send('( •_•) \n ( ง )ง \n /︶\\ eu e tu...eu e tu ');
+        return;
+    case `${prefix}vemprofut`:
+        message.channel.send(`Vem pro fut vem⚽Vem pro fut ⚽#VemProFut ⚽😎O futebol óh⚽tá estralando⚽😎tá pegado ò🤙✌️só tapa👋🤞só tapa de qualidade monstra🦕🐉aí o desafio Thiago Nere🤙🤙⚽é nós👉tamo junto⚽😎sempre🤙✌️lado a lado⚽aí eu tô de seleção⚽a satisfação aqui é total 😎🤙tamo junto⚽Vem`)
+        return;
+    case `${prefix}digdon`:
+        message.channel.send(`Morre de ré :red_car: :boom: `);
+        return;
+    default: 
+        return;
+    }
+});
 
 bot.login(botconfig.token);
