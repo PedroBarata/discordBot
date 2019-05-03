05fd6bf157f67fa96b7498162d813b0f00cfef46
pedrobarata <pedrofelipebarata@gmail.com>
2019-05-03 16:11:07 -0300

    feat: Comandos paulao, sushi e padeiro feitos

--- index.js
+++ index.js
@@ -4,27 +4,37 @@ const Discord = require("discord.js");
 const bot = new Discord.Client({ disableEveryone: true });
 
 bot.on("ready", async () => {
-  console.log(`${bot.user.username} is online!`);
+    console.log(`${bot.user.username} is online!`);
 });
 
 bot.on("message", async message => {
     const prefix = botconfig.prefix;
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  switch (cmd) {
-    case `${prefix}vemtranquilo`: 
-        message.channel.send("( •_•) \n ( ง )ง \n /︶\\ vem tranquilo, vem tranquilo...");
-        message.channel.send('( •_•) \n ( ง )ง \n /︶\\ vem afobado não... vem tranquilo');
-        message.channel.send('( •_•) \n ( ง )ง \n /︶\\ eu e tu...eu e tu ');
-        return;
-    case `${prefix}vemprofut`:
-        message.channel.send(`Vem pro fut vem⚽Vem pro fut ⚽#VemProFut ⚽😎O futebol óh⚽tá estralando⚽😎tá pegado ò🤙✌️só tapa👋🤞só tapa de qualidade monstra🦕🐉aí o desafio Thiago Nere🤙🤙⚽é nós👉tamo junto⚽😎sempre🤙✌️lado a lado⚽aí eu tô de seleção⚽a satisfação aqui é total 😎🤙tamo junto⚽Vem`)
-        return;
-    case `${prefix}digdon`:
-        message.channel.send(`Morre de ré :red_car: :boom: `);
-        return;
-    default: 
-        return;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    switch (cmd) {
+        case `${prefix}vemtranquilo`:
+            message.channel.send("( •_•) \n ( ง )ง \n /︶\\ vem tranquilo, vem tranquilo...");
+            message.channel.send('( •_•) \n ( ง )ง \n /︶\\ vem afobado não... vem tranquilo');
+            message.channel.send('( •_•) \n ( ง )ง \n /︶\\ eu e tu...eu e tu ');
+            return;
+        case `${prefix}vemprofut`:
+            message.channel.send(`Vem pro fut vem⚽Vem pro fut ⚽#VemProFut ⚽😎O futebol óh⚽tá estralando⚽😎tá pegado ò🤙✌️só tapa👋🤞só tapa de qualidade monstra🦕🐉aí o desafio Thiago Nere🤙🤙⚽é nós👉tamo junto⚽😎sempre🤙✌️lado a lado⚽aí eu tô de seleção⚽a satisfação aqui é total 😎🤙tamo junto⚽Vem`)
+            return;
+        case `${prefix}digdon`:
+            message.channel.send(`Morre de ré :red_car: :boom: `);
+            return;
+        case `sushi`:
+            message.channel.send(`:sushi:`);
+            return;
+        case `${prefix}paulao`:
+            message.channel.send(`Eu chamei, vcs que não vieram :shrug:`);
+            return; 
+            case `${prefix}padeiro`:
+            message.channel.send(`Aqueeela padeirada!`);
+            message.channel.send(`https://www.youtube.com/watch?v=-2tZqyhWG4o`);
+            return; 
+        default:
+            return;
     }
 });
 
