e4704632aab13bddcfa83ae901fe57fb2ce40775
Pedro Barata <pedro.barata@fasolti.com.br>
2020-04-01 16:14:56 -0300

    fix: fix cmd paulao 3

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ bot.on("message", async message => {
     return message.channel.send("https://media.discordapp.net/attachments/257651525453611009/685955532258803787/unknown.png")
   }
   if (cmd === `${prefix}paulao3`) {
-    return message.channel.send("Mas eu sou XERIFE :cowboy:")
+    return message.channel.send("Mas eu sou XERIFE :cowboy: :star:")
   }
 
 });
